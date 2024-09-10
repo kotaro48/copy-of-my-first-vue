3b9025d123f47a03b08b2001a158004c02bab56b
kotaro48 <88866545+kotaro48@users.noreply.github.com>
2024-09-10 15:38:29 +0900

    稍微改了代码,提交并merge到主分支

--- src/component/testProp.vue
+++ src/component/testProp.vue
@@ -11,10 +11,26 @@
 
 
 <script  lang="ts" setup name="TestProp">
-import { defineProps  } from 'vue';
+import type { Persons } from '@/types';
+import { defineProps } from 'vue';
+import { withDefaults } from 'vue';
 
     // 这里和<li v-for="item in personList 的 personList虽然指向同一个对象 但是标签里的名字由源头标签决定
-    let pList = defineProps(['personList']);
+    // 接受personList 不限制类型
+    //let pList = defineProps(['personList']);
+
+    // 接受personList并限制类型 
+    let pList = defineProps<{personList:Persons}>();
+
+    // 接受personList并限制类型 并限制必要性
+    let list2 = defineProps<{personList?:Persons}>();
+
+    // 接受personList并限制类型 并限制必要性 并指定默认值 但是必须是函数返回一个值
+    let list3 = withDefaults(defineProps<{personList?:Persons}>(), {
+        //personList:[{id:'00000',name:'staff',age:30},{id:'00001',name:'staff2',age:30},{id:'00002',name:'staff3',age:30}]
+        personList:() => [{id:'00000',name:'staff',age:30},{id:'00001',name:'staff2',age:30},{id:'00002',name:'staff3',age:30}]
+    });
+
 
 
 </script>
